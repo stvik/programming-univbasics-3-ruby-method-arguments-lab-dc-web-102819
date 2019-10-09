0447fe5a6f143c9501fbbc9c2a9600ea5ad9139e
Sara Tarnvik <stvik2@gmail.com>
2019-10-09 17:06:21 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -13,7 +13,8 @@ def introduction_with_language(name,language)
 end
 
 def introduction_with_language_optional(name, language = "Ruby")
-  
+  put "Hi, my name is #{name} and I am learning to program in #{language}"
+end
 
 
 
